5186c06456775532c6621d5cb1723e85fb5928a3
Sylvia Bouboutou <sylviabouboutou@hotmail.com>
2019-03-26 19:48:46 +0800

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Article.create(title: Faker::Quote.yoda, content: Faker::Lorem.paragraph)
+end
+
+puts "created #{Article.count} articles"
